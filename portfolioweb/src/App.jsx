13e63ef63591356fdb9475a9d5e3ee00c76d7600
LarrieM <larriemoses@gmail.com>
2024-09-02 12:12:12 -0700

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   portfolioweb/.gitignore 	new file:   portfolioweb/README.md 	new file:   portfolioweb/eslint.config.js 	new file:   portfolioweb/index.html 	new file:   portfolioweb/package-lock.json 	new file:   portfolioweb/package.json 	new file:   portfolioweb/public/vite.svg 	new file:   portfolioweb/src/App.css 	new file:   portfolioweb/src/App.jsx 	new file:   portfolioweb/src/components/Navbar.jsx 	new file:   portfolioweb/src/main.jsx 	new file:   portfolioweb/src/pages/Experience.jsx 	new file:   portfolioweb/src/pages/Home.jsx 	new file:   portfolioweb/src/pages/Projects.jsx 	new file:   portfolioweb/src/styles/Navbar.css 	new file:   portfolioweb/vite.config.js

---
+++ portfolioweb/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Home from './pages/Home';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+     <Router>
+        <Navbar />
+        <Routes>
+          <Route path ="/" element={<Home />} />
+          <Route path ="/projects" element={<Projects />} />
+          <Route path ="/experience" element={<Experience />} />
+        </Routes>
+     </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ portfolioweb/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
+import ReorderIcon from ''
+
+
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='toggleButton'>
+                <button></button>
+            </div>
+            <div className='Links'>
+                <Link to='/'> Home </Link>
+                <Link to='/projects'> Projects </Link>
+                <Link to='/experience'> Experience </Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
