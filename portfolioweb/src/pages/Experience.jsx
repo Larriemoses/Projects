96d0501b2c7165018e8c5c658fd71e19e374ff4f
LarrieM <larriemoses@gmail.com>
2024-09-12 16:11:12 +0000

    modified:   portfolioweb/package-lock.json 	modified:   portfolioweb/package.json 	modified:   portfolioweb/src/pages/Experience.jsx 	deleted:    portfolioweb/src/react-vertical-timeline-component.d.ts 	new file:   portfolioweb/src/styles/Experience.css 	modified:   portfolioweb/vite.config.js

--- portfolioweb/src/pages/Experience.jsx
+++ portfolioweb/src/pages/Experience.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from "@mui/icons-material/School"
-
+import '../styles/Experience.css'
 
 function Experience() {
   return (
     <div className="experience">
-      <VerticalTimeline lineColor='#3e497a'>
+      <VerticalTimeline className='vtimeline' lineColor='#3e497a'>
         <VerticalTimelineElement className='vertical-timeline-element-education' date="2010 - 2014" iconStyle={{background: '#3e497a', color: "#fff "}} icon={<SchoolIcon />}> </VerticalTimelineElement>
-      </VerticalTimeline></div>
+            <h3 vertical-timeline-element-title>Gbabijo High School, Gbabijo, Ondo State</h3>
+      <p>High School Disploma</p>
+      </VerticalTimeline>
+
+      
+      
+      
+      
+      </div>
   )
 }
 
