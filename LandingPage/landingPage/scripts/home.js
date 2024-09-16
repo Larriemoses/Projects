c3449a9409cc68551dc14970b2b98a4fb508ec97
LarrieM <larriemoses@gmail.com>
2024-09-16 19:18:21 +0000

    new file:   LandingPage/landingPage/assets/img1.jpg 	new file:   LandingPage/landingPage/assets/pexels-markusspiske-193350.jpg 	new file:   LandingPage/landingPage/pages/SoftwareDev.html 	new file:   LandingPage/landingPage/pages/home.html 	new file:   LandingPage/landingPage/scripts/home.js 	new file:   LandingPage/landingPage/styles/SoftwareDev.css 	new file:   LandingPage/landingPage/styles/home.css 	modified:   portfolioweb/package-lock.json 	modified:   portfolioweb/package.json

---
+++ LandingPage/landingPage/scripts/home.js
@@ -0,0 +1,46 @@
+
+// Function to display the modal
+
+function showMenu() {
+    // document.getElementsById("Navbar").style.height = "100px";
+    document.getElementById("Navbar_viewport").style.display = "block";
+ 
+  
+    // document.getElementById("externalContent").src = "/landingPage/pages/SoftwareDev.html"; // External page URL
+  }
+  function closeMenu() {
+    // document.getElementsById("Navbar").style.height = "100px";
+    document.getElementById("Navbar_viewport").style.display = "none";
+ 
+  
+    // document.getElementById("externalContent").src = "/landingPage/pages/SoftwareDev.html"; // External page URL
+  }
+
+  function showOurServices() {
+  
+    document.getElementById("Navbar_viewport").style.display = "none";
+
+
+
+  }
+
+
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function showModal1() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.transition = "ease-in-out 0.5s";
+
+  document.getElementById("externalContent").src = "/landingPage/pages/SoftwareDev.html"; // External page URL
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("externalContent"); // Reset iframe content
+}
