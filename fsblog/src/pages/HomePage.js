d76307b1dc7c6b165c1f64172a3d026c4e0ac61b
LarrieM <larriemoses@gmail.com>
2024-11-15 00:38:57 +0100

    modified:   fsblog/src/pages/HomePage.js
    Changes not staged for commit:
    	modified:   fsblog/src/App.css
    	modified:   fsblog/src/App.js
    	modified:   fsblog/src/pages/About.js
    	modified:   fsblog/src/pages/NotFoundPage.js
    
    Untracked files:
    	E-Commerce_web/
    	fsblog/my-blog-backend/
    	sendme/

--- fsblog/src/pages/HomePage.js
+++ fsblog/src/pages/HomePage.js
@@ -10,6 +10,16 @@ function HomePage() {
 
     <p>Hi there! I'm excited to have you here. This blog is where I share my journey through coding, software development, and my love for creative pursuits like music and art. Whether you're here to learn, get inspired, or just explore something new, I hope you'll find value in the content I create. Join me as we grow, learn, and tackle challenges together. Let's connect and make this space a hub for curious minds!</p>
     
+    <b className='titlehere'>What You Can Expect Here:</b>
+      <ul>
+        <li>Tech & Coding: In-depth articles and tutorials on web development, focusing on front-end frameworks like ReactJS, coding practices, and software engineering tips.</li>
+        <li>Creative Pursuits: Stories behind my music, insights into how I approach songwriting, and artwork that I’m excited to share.</li>
+        <li>Personal Growth & Inspiration: As someone passionate about self-improvement and fulfilling my purpose, I’ll share thoughts and experiences that have helped me grow both personally and professionally.</li>
+
+      </ul>
+      <p>Whether you’re here to learn something new, get inspired, or simply explore different facets of life through my lens, I’m so glad to have you along for the ride. Let’s embark on this journey together, filled with creativity, growth, and meaningful connections.</p>
+      <p>Feel free to comment, share, and engage with the content. Let’s build a community where we can all inspire and support one another!</p>
+      <p>Thank you for being here.</p>
     </div>
   </>
 
