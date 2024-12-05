bb6f02ae4f67257d25b028b6cb7484b1711411d7
Olarewaju Adebulu <80601652+Larriemoses@users.noreply.github.com>
2024-12-05 13:18:57 +0000

    modified:   E-Commerce_web/BlogApp/frontend/index.html 	modified:   E-Commerce_web/BlogApp/frontend/src/components/NavBar.jsx

--- E-Commerce_web/BlogApp/frontend/src/components/NavBar.jsx
+++ E-Commerce_web/BlogApp/frontend/src/components/NavBar.jsx
@@ -92,6 +92,20 @@ function NavBar() {
                   <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
                     Testimonials
                   </h2>
+
+                  <figure className="my-12">
+                    
+                    <blockquote className="bg-teal-600 dark:bg-black pl-14 pr-8 py-12 rounded-3xl relative">
+                     <p className="text-2xl sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:font-serif before:content-['\201C'] before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2 before:translate-y-2  after:font-serif after:content-['\201D'] after:absolute after:-bottom-20 after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-y-2 after:translate-x-2">
+                          AOM
+                          has always been there for me. Their Explorer rocket arrived in a wooden crate as expected. Life-long customer! A++ shopping experience. 
+                     </p>
+                     <figcaption className="italic text-xl sm:text-2xl text-right mt-2 text-slate-500 dark:text-slate">
+                      &#8212; Wile E. Coyote, Genius
+                     </figcaption>
+                    </blockquote>
+                  </figure>
+                  
                 </section>
                 
                 <hr className="mx-auto bg-black dark:bg-white w-1/2" />
