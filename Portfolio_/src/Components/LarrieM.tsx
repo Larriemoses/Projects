f7325066aeaa4d800706b25c7e2e93313bb0b339
larriemoses <larriemoses@gmail.com>
2025-02-25 23:59:55 +0100

    modified:   Portfolio_/src/App.tsx 	modified:   Portfolio_/src/Components/LarrieM.tsx 	modified:   Portfolio_/src/Components/Nav.tsx

--- Portfolio_/src/Components/LarrieM.tsx
+++ Portfolio_/src/Components/LarrieM.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
   <>
-  <div className=' bg-black w-full h-[100%] text-white'>
+  <div className=' bg-black w-full text-white h-[100vh]'>
     <h2>Larrie M</h2>
     <p>Full Stack Developer</p>
   </div>
--- Portfolio_/src/Components/Nav.tsx
+++ Portfolio_/src/Components/Nav.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFigma } from "react-icons/fa";
+// import { FaFigma } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin} from "react-icons/fa";
 import { FaMedium} from "react-icons/fa";
@@ -11,23 +11,21 @@ import { Link } from 'react-router-dom'
 function Nav() {
   return (
     <>
-
-    <div className="mr-5 hover:text-white"></div>
-    
-        <nav className='w-full bg-black text-white h-[10vh]  content-center justify-items-center '>
-          <ul className='flex flex-row content-center my-5 justify-center text-center items-center text-purple-400 bg-gray-900 max-w-max h-[70%] px-2 rounded-4xl '>
+      <nav className=' w-full bg-black text-white h-fit   font-sans content-center justify-items-center '>
+          <ul className='invisible md:visible flex flex-row py-4  content-center my-5 justify-center text-center items-center text-purple-400 bg-gray-900 max-w-max h-[70%] px-2 rounded-4xl '>
             <li className='flex mr-5  hover:text-white justify-center text-center justify-items-center '><Link className="justify-center" to="/">Larrie Moses</Link></li>
             <li className='mr-5  hover:text-white'><Link to="/education">Education</Link></li>
             <li className='mr-5  hover:text-white'><Link to="/experience">Experience</Link></li>
-            <li className='mr-2  hover:text-white content-center justify-items-center '><Link to="/#"><FaFigma size={15}/></Link></li>
+            {/* <li className='mr-2  hover:text-white content-center justify-items-center '><Link to="/#"><FaFigma size={15}/></Link></li> */}
             <li className='mr-2  hover:text-white content-center'><Link to="/#"><FaGithub size={15}/></Link></li>
-            <li className='mr-2  hover:text-white content-center text-center '><Link to="/#"><FaLinkedin size={15}/></Link></li>
+            <li className='mr-2  hover:text-white content-center text-center '><Link to="https://www.linkedin.com/in/olarewaju-adebulu-320184212/"><FaLinkedin size={15}/></Link></li>
             <li className='mr-2  hover:text-white content-center'><Link to="/#"><FaMedium size={15}/></Link></li>
             <li className='mr-2  hover:text-white content-center '><Link to="/#"><FaEnvelope size={15}/></Link></li>
             
-
           </ul>
         </nav>
+
+
       
     </>
   )
