d281589f81c1aea35a13aa16a99733968bb01825
larriemoses <larriemoses@gmail.com>
2025-03-25 12:56:20 +0100

    modified:   Portfolio_/src/Components/LarrieM.tsx

--- Portfolio_/src/Components/LarrieM.tsx
+++ Portfolio_/src/Components/LarrieM.tsx
@@ -7,10 +7,12 @@ import { FaEnvelope } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import image01 from "../assets/New Project (1).png";
 
+import experience from "./Experience";
+
 function Home() {
   return (
     <>
-      <div className="bg-black w-full">
+      <div className="bg-black w-full overflow-hidden">
         <nav className=" w-full text-white h-[40%]  mb-15 font-sans content-center justify-items-center ">
           <ul className="invisible md:visible flex flex-row py-4  content-center my-5 justify-center text-center items-center text-purple-400 bg-gray-900 max-w-max h-[70%] px-2 rounded-4xl ">
             <li className="flex mr-5  hover:text-white justify-center text-center justify-items-center ">
@@ -32,6 +34,7 @@ function Home() {
             </li>
             <li className="mr-3  hover:text-white content-center text-center ">
               <Link to="https://www.linkedin.com/in/olarewaju-adebulu-320184212/">
+                1
                 <FaLinkedin size={15} />
               </Link>
             </li>
@@ -49,41 +52,52 @@ function Home() {
         </nav>
 
         <div className="  bg-black w-full text-white h-[100vh] justify-center justify-items-center">
-          <div className="sm:flex flex-row-reverse w-[80%]">
-            <div className="sm:flex-1  justify-center justify-items-center sm:justify-center sm:justify-items-center items-center  ">
+          <div className="sm:flex flex-row-reverse w-[80%] sm:w-[85%]">
+            <div className="sm:flex-1  justify-center justify-items-center sm:justify-end sm:justify-items-center items-center  ">
               <img
                 src={image01}
                 alt=""
-                className="h-50 sm:h-70 flex sm:w-[45%] mb-4 sm:mb-0"
+                className="h-50 sm:h-60 flex mb-4 sm:mb-0"
               />
             </div>
-            <div className="sm:flex-1 justify-center justify-items-center sm:justify-items-start sm:justify-start items-center align-items-center">
+            <div className="sm:flex-1 justify-center justify-items-center sm:justify-items-start sm:justify-start items-center sm:items-center">
               <h2 className="text-4xl mb-2  ">Hi, I'm Olarewaju</h2>
               <h5 className="mb-4 text-xs text-center sm:text-start text-purple-400 font-normal sm:text-sm sm:font-semibold">
                 Frontend Developer | Technical Writer | Content Creator
               </h5>
               <p className="text-gray-600 text-sm sm:text-[1em] text-center sm:text-start leading-loose mb-10 ">
                 I build scalable web apps with
-                <span className="text-white font-semibold"> React.js,</span>
-                <span className="text-white font-semibold"> TypeScript, </span>
+                <span className="text-gray-400 font-semibold"> React.js,</span>
+                <span className="text-gray-400 font-semibold">
+                  {" "}
+                  TypeScript,{" "}
+                </span>
                 and
-                <span className="text-white font-semibold">
+                <span className="text-gray-400 font-semibold">
                   {" "}
                   Tailwind CSS
                 </span>{" "}
                 while simplifying tech through
-                <span className="text-white font-semibold"> writing </span>
+                <span className="text-gray-400 font-semibold"> Writing </span>
                 and{" "}
-                <span className="text-white font-semibold">
-                  content creation
+                <span className="text-gray-400 font-semibold">
+                  Content Creation
                 </span>
                 .
               </p>
-              <div className="next_sec">
-                <button className="bg-white text-black p-2 rounded-full w-[10em]">
-                  {" "}
-                  Download CV
-                </button>
+              <div className="next_sec flex-wrap sm:flex justify-items-center">
+                <a href="#" className="">
+                  <button className="  bg-white text-black p-2 rounded-full w-[10em] mr-1 sm:mr-15 hover:bg-gray-300 cursor-pointer">
+                    {" "}
+                    Download CV
+                  </button>
+                </a>
+                <div
+                  className="mt-4 sm:mt-0 flex sm:flex-1 items-center text-purple-400 font-semibold hover:text-purple-300 "
+                  onClick={experience}
+                >
+                  See Experience
+                </div>
               </div>
             </div>
           </div>
