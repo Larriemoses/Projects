6cd699fe1cf1eda70224fe72b649fefaacbe8de1
Olarewaju Adebulu <80601652+Larriemoses@users.noreply.github.com>
2025-06-13 13:56:47 -0700

    Update AddArticle.tsx

--- Lm_Blog/frontend/src/pages/AddArticle.tsx
+++ Lm_Blog/frontend/src/pages/AddArticle.tsx
@@ -29,6 +29,9 @@ const AddArticle: React.FC = () => {
         }
     };
 
+
+
+
     return (
         <div className="max-w-2xl mx-auto p-4">
             <h1 className="text-3xl font-bold mb-4">Add New Article</h1>
