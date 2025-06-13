badaa8d169ae300fbb387cbcfe57166e83d4fbc8
Olarewaju Adebulu <80601652+Larriemoses@users.noreply.github.com>
2025-06-13 13:57:18 -0700

    Update Home.tsx

--- Lm_Blog/frontend/src/pages/Home.tsx
+++ Lm_Blog/frontend/src/pages/Home.tsx
@@ -20,6 +20,9 @@ const Home: React.FC = () => {
         fetchArticles();
     }, []);
 
+
+
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-4xl font-bold mb-6">Blog Articles</h1>
