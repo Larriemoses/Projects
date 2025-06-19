35c8924241532eb164a2f7e006329b09d6c325e4
Olarewaju Adebulu <80601652+Larriemoses@users.noreply.github.com>
2025-06-19 15:46:13 -0700

    Update ArticleDetail.tsx

--- Lm_Blog/frontend/src/pages/ArticleDetail.tsx
+++ Lm_Blog/frontend/src/pages/ArticleDetail.tsx
@@ -18,7 +18,7 @@ const ArticleDetail: React.FC = () => {
                 const data = await response.json();
                 setArticle(data);
             } catch (err) {
-                setError((err as Error).message); // Explicitly cast to Error
+                setError((err as Error).message); 
             } finally {
                 setLoading(false);
             }
